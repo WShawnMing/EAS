8d4be16ae4d5f70daa13d945fd8583ef1be036f4
WShawnMing <161991088+WShawnMing@users.noreply.github.com>
2026-09-15 17:22:55 +0800

    更新Benchmark

--- src/eas/core/kernels/attention/attention.py
+++ src/eas/core/kernels/attention/attention.py
@@ -17,7 +17,79 @@ def attention_torch(
 
 
 @triton.jit
-def FlashAttentionV0(Q,K,V,O, sm_scale : tl.constexpr ,M: tl.constexpr, N: tl.constexpr, D: tl.constexpr,BLOCK_D: tl.constexpr,BLOCK_M: tl.constexpr,
+def FlashAttention1V0(Q,K,V,O, sm_scale : tl.constexpr ,M: tl.constexpr, N: tl.constexpr, D: tl.constexpr,BLOCK_D: tl.constexpr,BLOCK_M: tl.constexpr,
+    BLOCK_N: tl.constexpr):
+
+    # LOG2E = 1.4426950408889634
+    # qk_scale = sm_scale * LOG2E
+    # 加载每个program的q矩阵
+    pid = tl.program_id(0) # M 维度
+    offset_M = tl.arange(0, BLOCK_M) + pid*BLOCK_M
+    offset_D = tl.arange(0, BLOCK_D)
+
+    MASK_Q =  (offset_M[:, None] < M) & (offset_D[None, :] < D)
+    MASK_O =  (offset_M[:, None] < M) & (offset_D[None, :] < D)
+
+    Q_ptr = Q + offset_M[:,None]*D + offset_D[None,:]
+    O_ptr = O + offset_M[:,None]*D + offset_D[None,:]
+
+    q = tl.load(Q_ptr,mask=MASK_Q,other=0.0)
+
+
+
+    # 每行的最大值
+    m_i = tl.full((BLOCK_M,), -float("inf"), tl.float32)
+    # 每行的迭代和
+    l_i = tl.zeros((BLOCK_M,), tl.float32)    
+    # 累加暂存器 [BM,D]      
+    acc = tl.zeros((BLOCK_M, BLOCK_D), tl.float32)
+    
+    for start_n in range(0, N, BLOCK_N):
+
+        # 开始载入每个迭代步的K和V
+        offset_N = start_n + tl.arange(0, BLOCK_N)
+        MASK_K = (offset_N[:, None] < N) & (offset_D[None, :] < D)
+        MASK_V = (offset_N[:, None] < N) & (offset_D[None, :] < D)
+
+        K_ptr = K + offset_N[:, None] * D + offset_D[None,:]
+        V_ptr = V + offset_N[:, None] * D + offset_D[None,:]
+
+
+        k = tl.load(K_ptr,mask=MASK_K,other=0.0)
+        v = tl.load(V_ptr,mask=MASK_V,other=0.0)
+
+        # Q @ K_t [BM, BN] 过滤掉最后几个无用的N
+        score = tl.dot(q, tl.trans(k)) *  sm_scale
+        score = tl.where(offset_N[None, :] < N,score,-float("inf"),)
+
+        # 计算单步 m 和 l
+        m_block = tl.max(score, axis=1) # [BM]
+
+        m_new = tl.maximum(m_i, m_block)  # [BM]
+
+        alpha = tl.exp(m_i - m_new) 
+
+        p = tl.exp(score - m_new[:, None]) #  [BM, BN]
+
+        l_i = l_i*alpha + tl.sum(p, axis=1)
+        m_i = m_new
+
+
+         
+        # 计算acc 
+        acc_block = tl.dot(p.to(v.dtype),v) 
+        acc =  acc * alpha[:, None] + acc_block
+
+
+    acc = acc / l_i[:,None]
+    
+    tl.store(O_ptr,acc,mask=MASK_O)
+
+     
+
+
+@triton.jit
+def FlashAttention1V1(Q,K,V,O, sm_scale : tl.constexpr ,M: tl.constexpr, N: tl.constexpr, D: tl.constexpr,BLOCK_D: tl.constexpr,BLOCK_M: tl.constexpr,
     BLOCK_N: tl.constexpr):
 
     LOG2E = 1.4426950408889634
@@ -91,7 +163,7 @@ def FlashAttentionV0(Q,K,V,O, sm_scale : tl.constexpr ,M: tl.constexpr, N: tl.co
 
 
 # Q, K, V, output are tensors on the GPU
-def solve(
+def solve_v0(
     Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor, output: torch.Tensor, M: int, N: int, d: int
 ):
     BLOCK_M = 32
@@ -104,7 +176,7 @@ def solve(
         triton.cdiv(M, BLOCK_M),
     )
 
-    FlashAttentionV0[grid](
+    FlashAttention1V0[grid](
         Q,
         K,
         V,
@@ -117,6 +189,40 @@ def solve(
         BLOCK_M=BLOCK_M,
         BLOCK_N=BLOCK_N,
     )
+
+
+def solve_v1(
+    Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor, output: torch.Tensor, M: int, N: int, d: int
+):
+    BLOCK_M = 32
+    BLOCK_N = 32
+    BLOCK_D = max(16, triton.next_power_of_2(d))
+
+    sm_scale = 1.0 / math.sqrt(d)
+    # 一个 program 负责 BLOCK_M 行 Q / O
+    grid = (
+        triton.cdiv(M, BLOCK_M),
+    )
+
+    FlashAttention1V1[grid](
+        Q,
+        K,
+        V,
+        output,
+        sm_scale,
+        M,
+        N,
+        d,
+        BLOCK_D=BLOCK_D,
+        BLOCK_M=BLOCK_M,
+        BLOCK_N=BLOCK_N,
+    )
+
+
+def solve(
+    Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor, output: torch.Tensor, M: int, N: int, d: int
+):
+    solve_v1(Q, K, V, output, M, N, d)
 
 
 def benchmark_attention(
@@ -134,12 +240,14 @@ def benchmark_attention(
     V = torch.randn((N, d), device="cuda", dtype=torch.float16)
 
     torch_output = torch.empty((M, d), device="cuda", dtype=torch.float16)
-    triton_output = torch.empty((M, d), device="cuda", dtype=torch.float16)
+    v0_output = torch.empty((M, d), device="cuda", dtype=torch.float16)
+    v1_output = torch.empty((M, d), device="cuda", dtype=torch.float16)
 
     return benchmark_all(
         {
             "torch": lambda: attention_torch(Q, K, V, torch_output, M, N, d),
-            "triton": lambda: solve(Q, K, V, triton_output, M, N, d),
+            "triton_exp": lambda: solve_v0(Q, K, V, v0_output, M, N, d),
+            "triton_exp2": lambda: solve_v1(Q, K, V, v1_output, M, N, d),
         },
         warmup=warmup,
         repeat=repeat,
